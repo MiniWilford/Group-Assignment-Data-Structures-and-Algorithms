5de341ef5a57f9e2383ee2395265a8a5ffef5404
MiniWilford <ktbtut@gmail.com>
2022-12-01 02:14:51 -0500

    Implement Gson interface into AccountReader.java for deserializing / serializing JSON file type. This will be later added into GUI.

--- src/main/java/com/bank/AccountReader.java
+++ src/main/java/com/bank/AccountReader.java
@@ -1,14 +1,13 @@
 package com.bank;
 
+import com.google.gson.*;
+
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class AccountReader {
-
-    public static void main(String[] args) {
-        createAccount();
-    }
+public class AccountReader implements JsonSerializer<Account>, JsonDeserializer<Account> {
 
     /**
      * Create Account(s) using a Try-Catch, reading accounts.json file in directory path
@@ -21,4 +20,29 @@ public class AccountReader {
             throw new RuntimeException(e);
         }
     } // end of CreateAccount method
+
+    /**
+     *
+     * @param account
+     * @param type
+     * @param jsonSerializationContext
+     * @return
+     */
+    @Override
+    public JsonElement serialize(Account account, Type type, JsonSerializationContext jsonSerializationContext) {
+        return null;
+    }
+
+    /**
+     *
+     * @param jsonElement
+     * @param type
+     * @param jsonDeserializationContext
+     * @return
+     * @throws JsonParseException
+     */
+    @Override
+    public Account deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        return null;
+    }
 }
