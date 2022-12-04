2273c61a331581638b1015fbcdac40ec2b445b79
MiniWilford <ktbtut@gmail.com>
2022-12-04 00:23:01 -0500

    Changes / Additions to AccountTests.java test(s)

--- src/test/java/com/bank/test/AccountTests.java
+++ src/test/java/com/bank/test/AccountTests.java
@@ -3,6 +3,8 @@ package com.bank.test;
 import com.bank.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -63,9 +65,30 @@ public class AccountTests {
 
     @Test
     public void reportTotalInterestEarnedInAllAccounts () {
+        // Get Total interest from all accounts
+        Savings savings = new Savings();
+        savings.setBalance(100);
+        savings.setPeriods(5);
+        savings.setInterest(5);
 
+        Checking checking = new Checking();
+        checking.setBalance(100);
+        checking.setPeriods(5);
+        checking.setInterest(5);
 
-        Account.computeInterest();
+        CertificateOfDeposit certificateOfDeposit = new CertificateOfDeposit();
+        certificateOfDeposit.setBalance(100);
+        certificateOfDeposit.setPeriods(5);
+        certificateOfDeposit.setInterest(5);
+
+        Stack<Account> accounts = new Stack<>();
+        accounts.add(savings);
+        accounts.add(checking);
+        accounts.add(certificateOfDeposit);
+
+        Account account = new Account();
+        account.computeInterest();
+
         assertEquals(81444.74, Account.computeInterest(), 1.0);
     }
 
